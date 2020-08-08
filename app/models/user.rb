b84586e3c469b806e385bb16d9303c59bd8fb527
aknokd <aknokd.814@gmail.com>
2020-08-08 17:35:54 +0900

    Rubocopによるコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :family_name, :first_name, :birthday, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: true }, format: { with: /@/}
+  validates :email, presence: true, uniqueness: { case_sensitive: true }, format: { with: /@/ }
   validates :password, presence: true, length: { minimum: 6 }, confirmation: true, format: { with: /\A[a-z\d]+\z/i }
   validates :password_confirmation, presence: true
   validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ン]+\z/ }
