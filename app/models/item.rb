71f7eda3d8c4812b50622462c0bf2ba3e7bab890
aknokd <aknokd.814@gmail.com>
2020-08-10 23:59:07 +0900

    アソシエーションとバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,34 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :shipping_region
+  belongs_to_active_hash :days_until_shipping
+
+  with_options presence: true do
+    validates :name
+    validates :image
+    validates :content
+    validates :category
+    validates :status
+    validates :price
+    validates :delivery_fee
+    validates :shipping_region
+    validates :days_until_shipping
+    validates :user_id
+  end
+
+  # 選択が「---」の場合は保存できないようにする
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :shipping_region_id
+    validates :days_until_shipping_id
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   with_options presence: true do
     validates :nickname
     validates :email,                 uniqueness: { case_sensitive: true },
@@ -16,5 +18,8 @@ class User < ApplicationRecord
     validates :family_name_kana,      format: { with: /\A[ァ-ン]+\z/ }
     validates :first_name_kana,       format: { with: /\A[ァ-ン]+\z/ }
     validates :birthday
+
   end
+  
+
 end
