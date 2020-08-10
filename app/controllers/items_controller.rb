3518d28473d54609a333450030ec2d1862004dcf
aknokd <aknokd.814@gmail.com>
2020-08-11 00:09:38 +0900

    newアクションとcreateアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,24 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :name, :image, :content, :category_id, :status_id, :price,
+      :delivery_fee_id, :shipping_region_id, :days_until_shipping_id
+    ).merge(user_id: current_user.id)
   end
 
 end
