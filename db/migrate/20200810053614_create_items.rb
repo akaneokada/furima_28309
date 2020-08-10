9f5fe40d93dc28918a412e603299c286c48a94f3
aknokd <aknokd.814@gmail.com>
2020-08-10 15:38:47 +0900

    itemsテーブルの作成

--- db/migrate/20200810053614_create_items.rb
+++ db/migrate/20200810053614_create_items.rb
@@ -1,7 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string     :name,                null: false
+      t.text       :image,               null: false
+      t.text       :content,             null: false
+      t.integer    :category,            null: false
+      t.integer    :status,              null: false
+      t.integer    :price,               null: false
+      t.integer    :delivery_fee,        null: false
+      t.integer    :shipping_region,     null: false
+      t.integer    :days_until_shipping, null: false
+      t.references :user_id,             forign_key: true
       t.timestamps
     end
   end
