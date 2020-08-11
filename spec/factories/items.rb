ff4e686653428e93c8e80a9eea09324a5049699f
aknokd <aknokd.814@gmail.com>
2020-08-11 20:39:42 +0900

    Fakerを使用せずにitemを用意

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name                   { Faker::Lorem.sentence }
-    content                { Faker::Lorem.sentence }
-    category_id            { Faker::Number.within(range: 2..11) }
-    status_id              { Faker::Number.within(range: 2..7) }
-    price                  { Faker::Number.within(range: 300..9_999_999) }
-    delivery_fee_id        { Faker::Number.within(range: 2..3) }
-    shipping_region_id     { Faker::Number.within(range: 2..48) }
-    days_until_shipping_id { Faker::Number.within(range: 2..4) }
+    name                   { "オムライス" }
+    content                { "オムライスです" }
+    category_id            { 2 }
+    status_id              { 2 }
+    price                  { 1000 }
+    delivery_fee_id        { 2 }
+    shipping_region_id     { 2 }
+    days_until_shipping_id { 2 }
     association :user
   end
 end
