6406d0ed36a99d817ded36b6a87e890667ad08b8
aknokd <aknokd.814@gmail.com>
2020-08-20 16:39:44 +0900

    createアクションの定義

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,4 +2,30 @@ class TransactionsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    @buyer = Buyer.new(price: buyer_params[:price], item_id: buyer_params[:item_id], user_id: current_user.id)
+    if @buyer.valid?
+      pay_item
+      @buyer.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+  
+  private
+  
+  def buyer_params
+    params.permit(:price, :token, :item_id)
+  end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: buyer_params[:price],
+      card: buyer_params[:token],
+      currency:'jpy'
+    )
+  end
 end
