b2d96aae03dce15e36bf21a3a7aebfa6eb6efa8a
aknokd <aknokd.814@gmail.com>
2020-08-20 17:38:00 +0900

    クレジット決済におけるトークンの生成

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -1,14 +1,15 @@
 function check(e) {
-  const itemPrice = document.getElementById('item-price')
-  const value = itemPrice.value
-  const addTaxPrice = document.getElementById("add-tax-price")
-  const tax = `${value * 0.1}`
-  addTaxPrice.textContent = tax
-  
-  const profit = document.getElementById("profit")
-  const sales = `${value * 0.9}`
-  profit.textContent = sales
-  
-  e.preventDefault();
-}
+  if(document.URL.match(/new/) || document.URL.match(/edit/)) {
+    const itemPrice = document.getElementById('item-price').value
+    const addTaxPrice = document.getElementById("add-tax-price")
+    const tax = `${itemPrice * 0.1}`
+    addTaxPrice.textContent = tax
+    
+    const profit = document.getElementById("profit")
+    const sales = `${itemPrice * 0.9}`
+    profit.textContent = sales
+    
+    e.preventDefault();
+  }
+};
 window.addEventListener('input', check);
