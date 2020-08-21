f02558b848be8da185e5396e4c33ac0c5d09e74d
aknokd <aknokd.814@gmail.com>
2020-08-21 11:21:52 +0900

    購入前に必ずログインをさせる

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,6 @@
 class TransactionsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+
   def index
     @item = Item.find(params[:item_id])
     @buyer = ItemBuyer.new
