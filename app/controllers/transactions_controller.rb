1948e82320a5fe4cc1b4cb5a5754cdac76a91594
aknokd <aknokd.814@gmail.com>
2020-08-21 11:11:33 +0900

    createアクションの修正

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,25 +1,27 @@
 class TransactionsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @buyer = ItemBuyer.new
   end
 
   def create
-    @buyer = Buyer.new(price: buyer_params[:price], item_id: buyer_params[:item_id], user_id: current_user.id)
+    @item = Item.find(params[:item_id])
+    @buyer = ItemBuyer.new(buyer_params)
     if @buyer.valid?
       pay_item
       @buyer.save
       return redirect_to root_path
     else
-      render 'index'
+      render :index
     end
   end
   
   private
   
   def buyer_params
-    params.permit(:price, :token, :item_id)
+    params.permit(:price, :token, :postal_code, :prefecture, :city,
+                  :house_number, :building_name, :phone_number, :item_id).merge(user_id: current_user.id)
   end
-  
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
