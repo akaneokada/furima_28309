0e3257513a857ea0bd53ce87a9661dd060d7cfde
aknokd <aknokd.814@gmail.com>
2020-08-24 11:15:50 +0900

    バリデーションの修正

--- app/forms/item_buyer.rb
+++ app/forms/item_buyer.rb
@@ -2,15 +2,17 @@ class ItemBuyer
   include ActiveModel::Model
   attr_accessor :price, :token, :item_id, :user_id, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number
 
+  POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/
+
   with_options presence: true do
     validates :token
     validates :item_id
     validates :user_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
-    validates :prefecture, numericality: { other_than: 0, message: 'Select' }
+    validates :postal_code, format: { with: POSTAL_CODE_REGEX, message: 'Input correctly' }
+    validates :prefecture
     validates :city
     validates :house_number
-    validates :phone_number, length: { maximum: 11 }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
   end
 
   def save
