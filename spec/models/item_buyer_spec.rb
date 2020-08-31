3890741012296cc3964b1b9ecef21b40f3611c81
aknokd <aknokd.814@gmail.com>
2020-08-31 19:09:41 +0900

    ItemBuyerモデルの単体テストを日本語に修正

--- spec/models/item_buyer_spec.rb
+++ spec/models/item_buyer_spec.rb
@@ -20,42 +20,42 @@ RSpec.describe ItemBuyer, type: :model do
       it 'クレジットカードの情報がないと購入できない' do
         @item_buyer.token = ''
         @item_buyer.valid?
-        expect(@item_buyer.errors.full_messages).to include("Token can't be blank")
+        expect(@item_buyer.errors.full_messages).to include('カード情報を入力してください')
       end
       it 'postal_codeが空だと購入できない' do
         @item_buyer.postal_code = nil
         @item_buyer.valid?
-        expect(@item_buyer.errors.full_messages).to include("Postal code can't be blank")
+        expect(@item_buyer.errors.full_messages).to include('郵便番号を入力してください')
       end
       it 'postal_codeにハイフンが入っていないと購入できない' do
         @item_buyer.postal_code = '1234567'
         @item_buyer.valid?
-        expect(@item_buyer.errors.full_messages).to include('Postal code Input correctly')
+        expect(@item_buyer.errors.full_messages).to include('郵便番号にハイフンを含めてください')
       end
       it 'prefectureが空だと購入できない' do
         @item_buyer.prefecture = nil
         @item_buyer.valid?
-        expect(@item_buyer.errors.full_messages).to include("Prefecture can't be blank")
+        expect(@item_buyer.errors.full_messages).to include('都道府県を入力してください')
       end
       it 'cityが空だと購入できない' do
         @item_buyer.city = nil
         @item_buyer.valid?
-        expect(@item_buyer.errors.full_messages).to include("City can't be blank")
+        expect(@item_buyer.errors.full_messages).to include('市区町村を入力してください')
       end
       it 'house_numberが空だと購入できない' do
         @item_buyer.house_number = nil
         @item_buyer.valid?
-        expect(@item_buyer.errors.full_messages).to include("House number can't be blank")
+        expect(@item_buyer.errors.full_messages).to include('番地を入力してください')
       end
       it 'phone_numberが空だと購入できない' do
         @item_buyer.phone_number = nil
         @item_buyer.valid?
-        expect(@item_buyer.errors.full_messages).to include("Phone number can't be blank")
+        expect(@item_buyer.errors.full_messages).to include('電話番号を入力してください')
       end
       it 'phone_numberにハイフンが入っていると購入できない' do
         @item_buyer.phone_number = '090-123-456'
         @item_buyer.valid?
-        expect(@item_buyer.errors.full_messages).to include('Phone number is invalid')
+        expect(@item_buyer.errors.full_messages).to include('電話番号は不正な値です')
       end
     end
   end
